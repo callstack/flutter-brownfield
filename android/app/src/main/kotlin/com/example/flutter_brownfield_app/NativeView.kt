c64fc73c0be2cf0c40d3e2a925252f693217ed7e
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-07-05 13:00:16 +0200

    Squashed commit of the following:
    commit 07c5a8e38baeaf5d57bc8f40cd8071311da93392
    Author: Maciej Jastrzębski <mdjastrzebski@gmail.com>
    Date:   Fri Jul 5 11:29:09 2024 +0200
    
        feat: react native in own activity
    
    commit 0bda1db0c680214d81ae040254f06a961e50826b
    Author: Maciej Jastrzębski <mdjastrzebski@gmail.com>
    Date:   Thu Jul 4 15:11:54 2024 +0200
    
        chore: add package.json

--- android/app/src/main/kotlin/com/example/flutter_brownfield_app/NativeView.kt
+++ android/app/src/main/kotlin/com/example/flutter_brownfield_app/NativeView.kt
@@ -1,6 +1,7 @@
 package com.example.flutter_brownfield_app
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.view.View
 import android.widget.TextView
@@ -22,5 +23,9 @@ internal class NativeView(context: Context, id: Int, creationParams: Map<String?
         textView.textSize = 72f
         textView.setBackgroundColor(Color.rgb(255, 0, 0))
         textView.text = "Native View: $text (id: $id)"
+        textView.setOnClickListener {
+            val intent = Intent(context, ReactActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
---
+++ android/app/src/main/kotlin/com/example/flutter_brownfield_app/ReactActivity.kt
@@ -0,0 +1,85 @@
+package com.example.flutter_brownfield_app
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.KeyEvent
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
+
+class ReactActivity : Activity(), DefaultHardwareBackBtnHandler {
+	private var mReactRootView: ReactRootView? = null
+	private var mReactInstanceManager: ReactInstanceManager? = null
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		SoLoader.init(this, false)
+
+		mReactRootView = ReactRootView(this)
+		val packages: List<ReactPackage> = PackageList(application).getPackages()
+
+		mReactInstanceManager = ReactInstanceManager.builder()
+			.setApplication(application)
+			.setCurrentActivity(this)
+			.setBundleAssetName("index.android.bundle")
+			.setJSMainModulePath("index")
+			.addPackages(packages)
+			.setUseDeveloperSupport(BuildConfig.DEBUG)
+			.setInitialLifecycleState(LifecycleState.RESUMED)
+			.build()
+		mReactRootView!!.startReactApplication(mReactInstanceManager, "ReactNativeScreen", null)
+
+		setContentView(mReactRootView)
+	}
+
+	override fun onPause() {
+		super.onPause()
+
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager!!.onHostPause(this)
+		}
+	}
+
+	override fun onResume() {
+		super.onResume()
+
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager!!.onHostResume(this, this)
+		}
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager!!.onHostDestroy(this)
+		}
+		if (mReactRootView != null) {
+			mReactRootView!!.unmountReactApplication()
+		}
+	}
+
+	override fun onBackPressed() {
+		if (mReactInstanceManager != null) {
+			mReactInstanceManager!!.onBackPressed()
+		} else {
+			super.onBackPressed()
+		}
+	}
+
+	override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+		if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+			mReactInstanceManager!!.showDevOptionsDialog()
+			return true
+		}
+		return super.onKeyUp(keyCode, event)
+	}
+
+	override fun invokeDefaultOnBackPressed() {
+		super.onBackPressed()
+	}
+}
