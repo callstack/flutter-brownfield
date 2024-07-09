0d87e4eec89ee258db1bac39c9ac103fad3f2174
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-07-09 12:51:01 +0200

    feat: basic ios integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,10 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    guard let pluginRegisterar = self.registrar(forPlugin: "plugin-name") else { return false }
+    let factory = FLNativeViewFactory(messenger: pluginRegistrar.messenger())
+    pluginRegistrar.register(factory, withId: "native_view_ios")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
